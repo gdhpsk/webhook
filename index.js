db005b4dbd60bc5f7e361bde2e60eb07c6b42741
gdhpsk <70782191+gdhpsk@users.noreply.github.com>
2024-08-12 15:10:04 -0700

    setup

--- index.js
+++ index.js
@@ -1,15 +1,56 @@
 const express = require("express")
 const app = express()
-let encoder = new TextEncoder();
+let repositories = require("./repositories.json");
+const { spawn } = require("child_process");
 
 var githubMiddleware = require('github-webhook-middleware')({
     secret: process.env.secret
   });
 
-app.post('/', githubMiddleware, (req, response) => {
+app.post('/', githubMiddleware, async (req, response) => {
     if (req.headers['x-github-event'] != 'push') return res.status(200).end();
     response.status(202).send('Accepted');
-    console.log(req.body)
+    let {body} = req
+    let repo = repositories[body?.repository?.id?.toString() || ""]
+    if(!repo) return;
+    await new Promise((resolve, reject) => {
+        let cmd = spawn("sudo", ["git", "pull"], {cwd: `/projects/${repo}`})
+        cmd.on("exit", () => {
+            console.log(`Pulled for ${repo}`)
+            resolve("")
+        })
+    })
+    await new Promise((resolve, reject) => {
+        let cmd = spawn("sudo", ["docker", "build", "-t", repo, "."], {cwd: `/projects/${repo}`, shell: true})
+        cmd.on("exit", () => {
+            console.log(`Built ${repo}`)
+            resolve("")
+        })
+    })
+    
+    let id = await new Promise((resolve, reject) => {
+        let container_cmd = `docker ps | grep "${repo}" | awk '{print $1}'`
+    let container = spawn("sudo", container_cmd.split(" "), {shell: true, cwd: "/"})
+    container.once("message", (e) => {
+        let container = e.toString().trim()
+        resolve(container)
+    })
+    })
+
+    let command = await new Promise((resolve, reject) => {
+        let run_cmd = `docker run --rm -v /var/run/docker.sock:/var/run/docker.sock:ro assaflavie/runlike ${id}`
+        let cmd = spawn("sudo", run_cmd.split(" "), {shell: true, cwd: "/"})
+        cmd.once("message", (e) => {
+            let cmd = e.toString().trim()
+            resolve(cmd)
+        })
+    })
+
+    let deploy = `docker stop ${id}; sudo docker remove ${id}; sudo ${command}`
+    let cmd = spawn("sudo", deploy.split(" "), {shell: true, cwd: "/"})
+    cmd.once("exit", (e) => {
+        console.log(`Successfully deployed ${repo}`)
+    })
 });
 
 let port = process.env.PORT || 3000
